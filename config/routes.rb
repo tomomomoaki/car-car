805f151252dec8fdd24067ac54b3e754585969fb
tomomomoaki <tomomomomorou333@gmail.com>
2020-10-26 11:11:56 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'cars#index'
-  resources :cars, only: :index
+  resources :cars, only: [:index, :new, :create] do
+    resources :tags, only: [:create]
+  end
 end
